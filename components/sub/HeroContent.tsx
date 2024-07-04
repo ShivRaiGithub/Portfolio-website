2d2f251f62f65b83d64919fa45195682161db402
Super User <root@DESKTOP-8F1JPKQ.>
2024-07-04 11:54:56 +0530

    centering

--- components/sub/HeroContent.tsx
+++ components/sub/HeroContent.tsx
@@ -8,12 +8,12 @@ const HeroContent = () => {
     <motion.div
       initial="hidden"
       animate="visible"
-      className="flex flex-row items-center justify-center px-20 mt-40 w-full z-[20]"
+      className="flex flex-row items-center justify-center text-center mt-40 w-full z-[20] p-10"
     >
-      <div className="h-full w-full flex flex-col gap-3 justify-center m-auto text-start">
+      <div className="h-full w-full flex flex-col gap-3 justify-center m-auto text-center">
         <motion.div
           variants={fadeIn(0.5)}
-          className="flex flex-col gap-6 mt-6 text-8xl font-bold text-white max-w-[1000px] w-auto h-auto"
+          className="flex flex-col mt-6 text-8xl font-bold text-white w-auto h-auto text-center"
         >
           <span>
             Hello! <br />
@@ -27,7 +27,7 @@ const HeroContent = () => {
 
         <motion.p
           variants={fadeIn(0.8)}
-          className="text-gray-400 text-2xl my-5 max-w-[600px]">
+          className="text-gray-400 flex-col items-center mx-auto text-center text-2xl my-5 max-w-[1000px] ">
           I am a college student passionate about blockchain development with skills in web development, graphic design, video editing, etc.
         </motion.p>
       </div>
